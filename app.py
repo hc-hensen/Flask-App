e14c01c1f7aebef2325b7cce10e3bf7f8232511f
hc-hensen <hc@hensen.com.hk>
2024-05-21 17:30:07 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+''' https://github.com/CodingCraftYT/Flask-App '''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
